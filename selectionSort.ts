49fca5a7dbc625e196047b3e792a630271ffb308
Unekwe Francis <unekwenchekwube@gmail.com>
2022-03-24 15:28:59 +0100

    added description for selection sort

--- selectionSort.ts
+++ selectionSort.ts
@@ -1,3 +1,6 @@
+//Selection sorting sorts a given array of number by looping through the array from the least index and then placing the least number in that iteration
+// in the current iteration index that it is om
+
 //Perform selection sorting on the given array of numbers and order them from least to highest
 
 let initialArray = [24, 9, 2, 0, 4, 345, 297];
