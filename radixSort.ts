5a78ce4f6dfbd66f627403bee9a0e6f6efcc1b72
Unekwe Francis <unekwenchekwube@gmail.com>
2022-09-18 16:00:28 +0100

    implemented the second helper function for radix sort that returns the length of a number passed

--- radixSort.ts
+++ radixSort.ts
@@ -6,4 +6,13 @@ function digitChecker(num: number, pos: number): number {
   return Math.floor(Math.abs(num) / Math.pow(10, pos)) % 10;
 }
 
-console.log(digitChecker(136789, 5));
+// console.log(digitChecker(136789, 5));
+
+//2. A function that returns the number of digits a number has
+
+function noOfDigits(num: number): number {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+console.log(noOfDigits(136789));
