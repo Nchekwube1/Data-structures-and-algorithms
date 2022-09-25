9f1ae4a434459281797313f5c81f25969885c364
Unekwe Francis <unekwenchekwube@gmail.com>
2022-09-25 15:34:07 +0100

    solved the twoSum algorithm

--- twoSum.ts
+++ twoSum.ts
@@ -1,5 +1,23 @@
 //Given an array of numbers, return the indexes of two numbers such that they add up to target
 
-function twoSum(inputs: number[], target: number) {}
+function twoSum(nums: number[], target: number) {
+  let prevNums: Record<string, number> = {};
 
-console.log(twoSum([9, 1, 2, 5, 6, 7], 10));
+  let length = nums.length;
+  for (let i = 0; i < length; i++) {
+    const currentNum = nums[i];
+
+    const remainder = target - currentNum;
+    const checkIndex = prevNums[remainder];
+
+    if (typeof checkIndex === "number") {
+      return [checkIndex, i];
+    } else {
+      prevNums[currentNum] = i;
+    }
+  }
+
+  return [];
+}
+
+console.log(twoSum([9, 1, 2, 8, 6, 7], 14));
