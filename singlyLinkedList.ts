23c71fb5d4f48805aa2cdf80b71d98bf6593f8a5
Unekwe Francis <unekwenchekwube@gmail.com>
2022-11-15 12:04:39 +0100

    overview of singly linked list

--- singlyLinkedList.ts
+++ singlyLinkedList.ts
@@ -1,3 +1,4 @@
 // A linked list consists of elements with no indicees who are just pointing to the next element. Linked lists consists of nodes and each
 // node has a value and a pointer to another node or null to signify the end of the singly linked list
 // Each node is only connected one directionally to the next node
+// You cannot randomly access a value in the linked list, you have to traverse the list as there is no index
